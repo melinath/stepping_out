41233b891d19702b2cb9fa7714b26ab9f5017fb1
Stephen Burrows <stephen.r.burrows@gmail.com>
2010-09-03 18:43:41 -0400

    Minor corrections to payment processor.

--- pricing/processing.py
+++ pricing/processing.py
@@ -23,34 +23,32 @@ def validate_item(ipn_obj):
 def process_payment(sender, **kwargs):
 	ipn_obj = sender
 	if validate_item(ipn_obj):
-		app_label, model, object_id, user_id = ipn_obj.item_number.split(':')
-		errors = False
+		app_label, model, object_id, user_id = ipn_obj.custom.split(':')
 		
 		try:
 			user = User.objects.get(id=user_id)
 		except User.DoesNotExist:
 			ipn_obj.set_flag("Payment attempted for user who does not exist: %s" % user_id)
-			errors = True
 		
 		try:
 			ct = ContentType.objects.get(app_label=app_label, model=model)
 		except ContentType.DoesNotExist:
 			ipn_obj.set_flag("Payment attempted for content type which does not exist: %s.%s" % (app_label, model))
-			errors = True
 		else:
 			try:
 				obj = ct.get_object_for_this_type(id=object_id)
 			except ct.model_class().DoesNotExist:
 				ipn_obj.set_flag("Payment attempted for %s with id %s which does not exist." % (ct.model_class().__name__, object_id))
-				errors = True
 		
-		if not errors:
+		# Should this process even if the ipn is flagged?
+		if not ipn_obj.flag:
 			payment = Payment(
-				payment_for=obj,
-				user=user,
-				paid=ipn_obj.mc_gross,
-				payment_method='online',
-				payment_made = datetime.datetime.now()
+				payment_for = obj,
+				user = user,
+				paid = ipn_obj.mc_gross,
+				payment_method = 'online',
+				payment_made = datetime.datetime.now(),
+				ipn = ipn_obj
 			)
 			payment.save()
 		else:
