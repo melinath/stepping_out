3087b88efec61ab779d857c1328cf175731ca2dd
Stephen Burrows <stephen.r.burrows@gmail.com>
2011-01-23 16:16:13 -0500

    Switched OfficerUserTermMetaInfo to Officer and otherwise tidied the auth models & admin.

--- auth/admin.py
+++ auth/admin.py
@@ -12,65 +12,50 @@ from stepping_out.mail.admin import UserEmailInline
 COLLAPSE_CLOSED_CLASSES = ('collapse', 'collapse-closed', 'closed',)
 
 
-class OfficerPositionInline(admin.TabularInline):
-	model = OfficerUserMetaInfo
-	filter_horizontal = ('terms',)
-	verbose_name = 'officer position'
-	verbose_name_plural = 'officer positions'
-	extra = 1
-	classes = COLLAPSE_CLOSED_CLASSES
+class OfficerForm(forms.ModelForm):
+	def __init__(self, *args, **kwargs):
+		super(OfficerForm, self).__init__(*args, **kwargs)
+		self.fields['start'].required = False
+		self.fields['end'].required = False
+	
+	class Meta:
+		model = Officer
+
+
+class OfficerAdmin(admin.ModelAdmin):
+	form = OfficerForm
+	raw_id_fields = ('position', 'user', 'term')
 
 
 class OfficerPositionAdmin(admin.ModelAdmin):
 	list_editable = ['order']
-	list_display = ['name', 'order'] 
-
-
-class TermAdminForm(forms.ModelForm):
-	class Meta:
-		model = Term
-	
-	def clean_end(self):
-		if(self.cleaned_data['end']<self.cleaned_data['start']):
-			raise forms.ValidationError('A term cannot end before it starts!')
-			
-		return self.cleaned_data['end']
+	list_display = ['name', 'order']
+	prepopulated_fields = {'slug': ('name',)}
 
 
 class TermAdmin(admin.ModelAdmin):
-	list_display = ['name', 'start', 'end']
-	form = TermAdminForm
+	list_display = ['__unicode__', 'start', 'end']
 
 
 class SteppingOutUserAdminForm(UserChangeForm):
-	def __init__(self, data=None, files=None, auto_id='id_%s', prefix=None,
-				initial=None, error_class=forms.util.ErrorList, label_suffix=':',
-				empty_permitted=False, instance=None):
-		if instance:
-			CHOICES = [(email.email, email.email) for email in instance.emails.all()]
+	def __init__(self, *args, **kwargs):
+		super(SteppingOutUserAdminForm, self).__init__(*args, **kwargs)
+		
+		if self.instance.pk:
+			CHOICES = [(email.email, email.email) for email in self.instance.emails.all()]
 		else:
 			CHOICES = ()
 		
-		self.base_fields['email'] = forms.CharField(
+		self.fields['email'] = forms.CharField(
 			label='Primary email',
 			widget=SelectOther(other=AdminEmailInputWidget, choices=CHOICES),
 			required=False,
 			validators = [validate_email]
-		) 
-		super(SteppingOutUserAdminForm, self).__init__(
-			data, files, auto_id, prefix, initial, error_class, label_suffix,
-			empty_permitted, instance
 		)
 
 
-USER_INLINES = [
-	UserEmailInline,
-	OfficerPositionInline
-]
-
-
 class SteppingOutUserAdmin(UserAdmin):
-	inlines = USER_INLINES + UserAdmin.inlines
+	inlines = [UserEmailInline,] + UserAdmin.inlines
 	fieldsets = (
 		(None, {
 			'fields': ('username', 'password')
@@ -101,4 +86,5 @@ User._meta.get_field('email')._unique = True
 admin.site.unregister(User)
 admin.site.register(User, SteppingOutUserAdmin)
 admin.site.register(OfficerPosition, OfficerPositionAdmin)
+admin.site.register(Officer, OfficerAdmin)
 admin.site.register(Term, TermAdmin)
--- auth/models.py
+++ auth/models.py
@@ -1,13 +1,14 @@
+#encoding: utf-8
 from django.db import models
 from django.db.models import PositiveIntegerField
 from django.contrib.auth.models import User, Permission
 from django.contrib.localflavor.us.models import PhoneNumberField
-from datetime import datetime
+import datetime
 
 
 __all__ = (
 	'Term',
-	'OfficerUserMetaInfo',
+	'Officer',
 	'OfficerPosition'
 )
 
@@ -16,55 +17,79 @@ PhoneNumberField(blank=True).contribute_to_class(User, 'phone_number')
 
 
 class TermManager(models.Manager):
-	def get_current(self):
-		return self.exclude(start__gt=datetime.now).exclude(end__lt=datetime.now)
+	def for_date(self, date):
+		self.exclude(start__gte=date).exclude(end__lte=date)
+	
+	def current(self):
+		return self.for_date(datetime.date.today())
 
 
 class Term(models.Model):
 	"""Describes a term of office."""
+	objects = TermManager()
+	
 	start = models.DateField()
 	end = models.DateField()
 	name = models.CharField(max_length=30, blank=True)
-	objects = TermManager()
-	positions = models.ManyToManyField('OfficerPosition', related_name="terms", through='OfficerUserTermMetaInfo')
-
+	positions = models.ManyToManyField('OfficerPosition', related_name="terms", through='Officer')
+	
+	def is_current(self):
+		return self.start <= datetime.date.today() <= self.end
+	
 	def __unicode__(self):
-		if(self.name):
+		if self.name:
 			return self.name
 
-		return "%s -> %s" % (self.start,self.end)
+		return "%s – %s" % (self.start,self.end)
 
 	class Meta:
 		app_label = 'stepping_out'
 
 
-class OfficerUserTermMetaInfo(models.Model):
-	officer_position = models.ForeignKey('OfficerPosition')
-	user = models.ForeignKey(User)
-	term = models.ForeignKey(Term)
+class OfficerManager(models.Manager):
+	def for_date(self, date):
+		return self.exclude(end__lt=date).exclude(start__gt=date)
+	
+	def current(self):
+		return self.for_date(datetime.date.today())
+
+
+class Officer(models.Model):
+	objects = OfficerManager()
+	
+	position = models.ForeignKey('OfficerPosition')
+	user = models.ForeignKey(User, related_name='officers')
+	term = models.ForeignKey(Term, related_name='officers')
 	
 	start = models.DateField()
 	end = models.DateField()
+
+	def __unicode__(self):
+		return '%s :: %s' % (self.user.get_full_name(), self.position)
+	
+	def clean(self):
+		if self.start and self.end:
+			if self.start < self.term.start or self.end > self.term.end:
+				raise ValidationError("An officer position cannot be held beyond its term.")
+			
+			if self.end <= self.start:
+				raise ValidationError("An officer position can't end before it starts.")
 	
 	def save(self, *args, **kwargs):
 		if self.start is None:
 			self.start = self.term.start
 		if self.end is None:
 			self.end = self.term.end
-		super(OfficerUserTermMetaInfo, self).save(*args, **kwargs)
+		super(Officer, self).save(*args, **kwargs)
 
 	class Meta:
-		verbose_name = 'Officer/User Meta Info'
-		verbose_name_plural = 'Officer/User Meta Info'
 		app_label = 'stepping_out'
-
-	def __unicode__(self):
-		return '%s :: %s' % (self.user.get_full_name(), self.officer_position)
+		unique_together = ('user', 'position', 'term')
 
 
 class OfficerPositionManager(models.Manager):
 	def get_user_positions(self, user, termlist):
-		return self.filter(users=user, users__officerusermetainfo__terms__in=termlist)
+		return self.filter(users=user, users__officers__terms__in=termlist)
 
 	def get_user_current_positions(self, user):
 		return self.get_user_positions(user, Term.objects.get_current())
@@ -78,27 +103,28 @@ class OfficerPosition(models.Model):
 	Conveniently, these positions can also be used to automatically generate a
 	list of current officers, through the extended manager.
 	"""
-	#TODO: Update the admin.
+	OFFICER = 'o'
+	INSTRUCTOR = 'i'
 	CATEGORY_CHOICES = (
-		('o', 'Officer'),
-		('i', 'Instructor'),
+		(OFFICER, 'Officer'),
+		(INSTRUCTOR, 'Instructor'),
 	)
 	objects = OfficerPositionManager()
 	
 	name = models.CharField(max_length=50)
-	slug = models.SlugField(max_length=50)
+	slug = models.SlugField(max_length=50, unique=True)
 	description = models.TextField(help_text="A description of the officer's roles and responsibilities.")
 	
 	permissions = models.ManyToManyField(Permission, blank=True, null=True)
 	order = models.IntegerField()
 	category = models.CharField(max_length=1, choices=CATEGORY_CHOICES)
-	users = models.ManyToManyField(User, related_name="officer_positions", through=OfficerUserTermMetaInfo)
+	users = models.ManyToManyField(User, related_name="officer_positions", through=Officer)
 
 	def __unicode__(self):
 		return self.name
 
 	def users_for_terms(self, termlist):
-		return self.users.filter(officerusermetainfo__terms__in=termlist)
+		return self.users.filter(officers__terms__in=termlist)
 
 	@property
 	def current_users(self):
